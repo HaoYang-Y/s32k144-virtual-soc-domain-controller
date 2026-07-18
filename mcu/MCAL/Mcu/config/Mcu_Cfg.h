6ab9c73b705483f564689ae5f558c14f61ca9676
纸箱丶战机 <970378809@qq.com>
2026-07-18 22:36:14 +0800

    refactor: SPI/UART时钟切到FIRC, 文档时钟方案统一
    代码改动: clock_config.c全换FIRCDIV2(48MHz), Mcu_Cfg.h新增宏
    文档改动: 01新增CAN为何不能FIRC, 删Loopback, SOSC升主角
    文档改动: 02新增Core/Bus/Slow物理时钟域解释, 全部图表更新为FIRC
    文档改动: 03标注已实施
    最终方案: FIRC(CPU+SPI+UART) + SOSC(CAN), 两个时钟源

--- mcu/MCAL/Mcu/config/Mcu_Cfg.h
+++ mcu/MCAL/Mcu/config/Mcu_Cfg.h
@@ -13,7 +13,8 @@
 
 #define MCU_CORE_CLK_FREQ_HZ     48000000UL   /**< 内核时钟 (FIRC 48MHz) */
 #define MCU_BUS_CLK_FREQ_HZ      48000000UL   /**< 总线时钟 */
-#define MCU_FLEXCAN0_CLK_FREQ_HZ 8000000UL    /**< FlexCAN0 PE 时钟 (SOSC 8MHz) */
+#define MCU_FLEXCAN0_CLK_FREQ_HZ 8000000UL    /**< FlexCAN0 PE 时钟 (SOSC 8MHz, CAN 真实通信) */
 #define MCU_SLOW_CLK_FREQ_HZ     4000000UL    /**< 慢速时钟 */
+#define MCU_FIRCDIV2_CLK_FREQ_HZ 48000000UL   /**< FIRCDIV2 = 48MHz, SPI/UART 外设时钟源 */
 
 #endif /* MCU_CFG_H */
